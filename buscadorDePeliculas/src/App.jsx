b2a66c7009907da14c95f716209a9f5bf9f90d89
Gorka Reguero <gorkareguero@Mac-mini-de-Gorka.local>
2023-10-30 12:00:02 +0100

    movies  mock data

--- buscadorDePeliculas/src/App.jsx
+++ buscadorDePeliculas/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/with-no-results.json'
 function App() {
-  const [count, setCount] = useState(0)
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
+  const renderMovies = () => {
+   return ( 
+      <ul>
+        {
+          movies.map(movie => (
+            <li key={movie.imdbID}>
+              <h3>{movie.Title}</h3>
+              <p>{movie.Year}</p>
+              <img src={movie.Poster} />
+
+            </li>
+          ))
+        }
+      </ul>
+     )
+    }
+const renderNoResuts = () => {
+  return (
+    <p> No se han encontrado peliculas</p>
+  )
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='page'>
+      <header>
+        <form className='form'>
+          <input type="text" placeholder='Avengers, Star Wars ...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+        {
+          hasMovies ? renderMovies() : renderNoResuts()
+        }
+      </main>
+    </div>
   )
 }
 
